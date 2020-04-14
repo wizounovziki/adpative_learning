8d74ef178d1d4c7b52d7635a2a0b6c2a593356fd
wizounovziki <42036658+wizounovziki@users.noreply.github.com>
2020-04-14 15:42:55 +0800

    Update app.py

--- badass_api/app.py
+++ badass_api/app.py
@@ -21,8 +21,7 @@ import cv2
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 app = Flask(__name__)
-app.config['EYE_GAZING_HOURGLASS'] = './models/eye_gazing/ELG_i180x108_f60x36_n64_m3/checkpoints/hourglass/'
-app.config['EYE_GAZING_RADIUS'] = './models/eye_gazing/ELG_i180x108_f60x36_n64_m3/checkpoints/radius/'
+
 app.config['EASY_EYE_GAZING'] = './models/distraction_model/shape_predictor_68_face_landmarks.dat'
 app.config['EMBEDDING_FOLDER'] = './storage/embeddings'
 app.config['META_FOLDER'] = './storage/identity_meta'
@@ -51,23 +50,19 @@ if not os.path.exists(app.config['IMAGE_UPLOAD_FOLDER']):
     os.makedirs(app.config['IMAGE_UPLOAD_FOLDER'])
 
 # initialize
-
+print(" * Loading face detection model")
+# ort_session = ort.InferenceSession(app.config['FACE_DETECTION_MODEL'])
 print(" * Loading face aligner")
+# shape_predictor = dlib.shape_predictor(app.config['FACIAL_LANDMARK'])
 print(' * Loading face recognition model')
-tf_session = tf.Session()
+
 print(" * Loading eye gazing model")
 eye_predictor = dlib.shape_predictor(app.config['EASY_EYE_GAZING'])
+
 print(" * Loading distraction model")
 emotion_model = load_model('./models/distraction_model/emotion_recognition.h5')
 detector = dlib.get_frontal_face_detector()
 faceCascade = cv2.CascadeClassifier('./models/distraction_model/haarcascade_frontalface_default.xml')
-
-# tf_session_hourglass = tf.Session()
-# saver_hourglass = tf.train.import_meta_graph(os.path.join(app.config['EYE_GAZING_HOURGLASS'], 'model-4203370.meta'))
-# saver_hourglass.restore(tf_session_hourglass,os.path.join(app.config['EYE_GAZING_HOURGLASS'], 'model-4203370'))
-# tf_session_radius = tf.Session()
-# saver_radius = tf.train.import_meta_graph(os.path.join(app.config['EYE_GAZING_RADIUS'], 'model-4203370.meta'))
-# saver_radius.restore(tf_session_radius,os.path.join(app.config['EYE_GAZING_RADIUS'], 'model-4203370'))
 
 print(' * Loading existing embeddings')
 embeddings_meta = []
@@ -86,15 +81,11 @@ if __name__ == '__main__':
     from apis import detect_face_api
     app.register_blueprint(detect_face_api)
 
-    # from api import track_face_api
-    # app.register_blueprint(track_face_api)
-
     from apis import recognize_face_api
     app.register_blueprint(recognize_face_api)
 
     from apis import eye_gazing_api
     app.register_blueprint(eye_gazing_api)
-    
     
     from apis import head_motion_api
     app.register_blueprint(head_motion_api)
@@ -117,17 +108,6 @@ if __name__ == '__main__':
     from apis import portrait_api
     app.register_blueprint(portrait_api)
 
-    # from apis import anxiety_level_api
-    # app.register_blueprint(anxiety_level_api)
-
-    # from apis import student_skill_level_api
-    # app.register_blueprint(student_skill_level_api)
-
-    # from apis import question_difficulty_api
-    # app.register_blueprint(question_difficulty_api)
-
-    # from apis import anxiety_correct_api
-    # app.register_blueprint(anxiety_correct_api)
 
     from apis import quiz_details_api
     app.register_blueprint(quiz_details_api)
@@ -142,5 +122,5 @@ if __name__ == '__main__':
     app.register_blueprint(distraction_api)
 
     # app.run(host='0.0.0.0', port=5000, debug=False,threaded=True)
-    serve(app, host='0.0.0.0', port=7777)
+    serve(app, host='0.0.0.0', port=5000)
     
